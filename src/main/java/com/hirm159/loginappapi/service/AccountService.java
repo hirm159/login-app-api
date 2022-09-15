a7c6743fc1160b222752af1c54d6995bd406f7a7
hirm159 <hiro159m@gmail.com>
2022-09-15 21:47:39 +0900

    アカウントハッシュ化、作成処理修正

--- src/main/java/com/hirm159/loginappapi/service/AccountService.java
+++ src/main/java/com/hirm159/loginappapi/service/AccountService.java
@@ -2,6 +2,7 @@ package com.hirm159.loginappapi.service;
 
 import java.sql.Timestamp;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -23,13 +24,11 @@ public class AccountService {
 	public Account accountInput(AccountInputDto input) {
 		Account inputAccount = new Account();
 		inputAccount.setUsername(input.getUsername());
-		inputAccount.setPassword(input.getPassword());
+		inputAccount.setPassword(createPassword(input.getUsername(), input.getPassword()));
 		inputAccount.setMailAddress(input.getMailAddress());
 
-		Timestamp nowTime = new Timestamp(System.currentTimeMillis());
-
-		inputAccount.setFirstDate(nowTime);
-		inputAccount.setLastDate(nowTime);
+		inputAccount.setFirstDate(nowTime());
+		inputAccount.setLastDate(nowTime());
 		inputAccount.setVersion(0);
 
 		accountRepository.save(inputAccount);
@@ -39,17 +38,35 @@ public class AccountService {
 
 	// 既存アカウントへの更新処理
 	public Account accountUpdate(AccountUpdateDto input) {
-		Account account = new Account();
-		
 		if (!accountRepository.existsById(input.getId())) {
 			return null;
 		}
-		
+
+		Account account = accountRepository.getReferenceById(input.getId());
+
 		account.setId(input.getId());
-		account.setPassword(input.getMailAddress());
+		account.setPassword(createPassword(account.getUsername(), input.getPassword()));
 		account.setMailAddress(input.getMailAddress());
+		account.setLastDate(nowTime());
+		account.setVersion(account.getVersion() + 1);
 
 		accountRepository.save(account);
 		return account;
 	}
+
+	// パスワード生成処理
+	private String createPassword(String username, String password) {
+		if (username != null && password != null) {
+			String newPassword = DigestUtils.md5Hex(username + password) ;
+			return newPassword;
+		}
+		return null;
+	}
+
+	// 時刻更新処理
+	private Timestamp nowTime() {
+		Timestamp nowTime = new Timestamp(System.currentTimeMillis());
+		return nowTime;
+	}
+
 }
